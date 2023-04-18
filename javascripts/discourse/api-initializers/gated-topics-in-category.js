5df49763790b9359bdfa1b6d5bbe6bb696ed0b30
Arkshine <github@arkshine.me>
2023-04-18 02:57:28 +0200

    FEATURE: Hide private groups if user is not the owner

---
+++ javascripts/discourse/api-initializers/gated-topics-in-category.js
@@ -0,0 +1,40 @@
+import { apiInitializer } from "discourse/lib/api";
+import { action } from "@ember/object";
+
+const PLUGIN_ID = "gated-topics-in-category";
+
+const allowedGroups = settings.allowed_groups
+  .split("|")
+  .map((id) => parseInt(id, 10))
+  .filter((id) => id);
+
+export default apiInitializer("0.11.1", (api) => {
+  const currentUser = api.getCurrentUser();
+
+  if (!currentUser || !currentUser.groups) {
+    return;
+  }
+
+  api.modifyClass("controller:groups-index", {
+    pluginId: PLUGIN_ID,
+
+    @action
+    loadMore() {
+      if (this.groups && this.groups.length > 0) {
+        const groupsToRemove = this.groups.filter((group) => {
+          return (
+            allowedGroups.includes(group.id) &&
+            group.isPrivate &&
+            !group.is_group_owner
+          );
+        });
+
+        if (groupsToRemove.length > 0) {
+          this.groups.removeObjects(groupsToRemove);
+        }
+      }
+
+      this.groups && this.groups.loadMore();
+    },
+  });
+});
